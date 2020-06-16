8586c831b1a4b79ccd0fa6fdc4244eca49055926
Your Name <you@example.com>
2020-06-16 12:54:01 +0200

    added instructions for file upload

--- fileUploader.R
+++ fileUploader.R
@@ -1,10 +1,33 @@
 fileUploader = fluidPage(
   mainPanel(
-    helpText("Upload a file"),
-    fileInput(inputId = "replace_inputs", label = "File", multiple = TRUE),
-    actionButton(inputId = "replaceFiles", "Update input files for the mode choice analytical calculation")
+    #fluidRow(
+    #  column(4,
+            br(),
+             h4("File Upload"),
+            helpText(HTML('Please ensure that all input data is provided as .csv-files and named correctly. The shape of analysis zones has to be specified in a shape file. It can be uploaded as a zipped file containing the shape file components: .dbf, .prj, .shp and .shx.')),
+            br(),
+            fluidRow(
+            column(5,
+                   helpText(HTML('The correct file names are as follows:<br><br> \'area.csv\' areas of zones <br>\'congestion.csv\' congestion factors<br>\'dxs.csv\' parcel densities for XS<br>\'ds.csv\' parcel densities for S<br>\'dm.csv\' parcel densities for M<br>\'dl.csv\' parcel densities for L<br>\'zones.zip\' shapefile of zones'))
+            ),
+            column(7,
+             helpText(HTML("In order to determine a mode choice for the uploaded input, browse and select one or more files, click on \'Confirm upload\' and conduct a new Mode Choice Optimization ")),
+             fileInput(inputId = "replace_inputs", label = "File", multiple = TRUE),
+             actionButton(inputId = "replaceFiles", "Confirm upload"),
+             br(),
+             )
+            )
+  )
+    #  column(8,
+    #        br(),
+    #        br(),
+    #        br(),
+    #        br(),
+      
+    #  )
+    #)
 )
-)
+#)
 
 serverUploader = function(input, output, session){
  }
